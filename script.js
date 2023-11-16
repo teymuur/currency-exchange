efbb1cd0045c341c65634ac1372b77b0cabe16aa
teymuur <teymur_babayev08@yahoo.com>
2023-11-16 17:57:52 +0400

    Bug fixed

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function getExchangeRate() {
 
 
     const apiKey = 'YOUR_API_KEY';
-    const apiUrl = `https://open.er-api.com/v6/latest/${fromCurrency.value}`;
+    const apiUrl = `https://open.er-api.com/v6/latest/${fromCurrency.value}?apiKey=${apiKey}`;
 
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        const rate = data.rates[toCurrency];
-        const result = amount * rate;
-        document.getElementById('result').innerText = `${amount} ${fromCurrency} is ${result.toFixed(2)} ${toCurrency}`;
+        const rate = data.rates[toCurrency.value];
+        const result = amount.value * rate;
+        document.getElementById('result').innerText = `${amount.value} ${fromCurrency.value} is ${result.toFixed(2)} ${toCurrency.value}`;
       })
       .catch(error => console.error('Error fetching exchange rates:', error));
   }
