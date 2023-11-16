87fb6b7ca59c19af8a3c76024fb6f2881a8df6fc
teymuur <teymur_babayev08@yahoo.com>
2023-11-16 17:54:55 +0400

    Updated index.html and script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const fromCurrency = document.getElementById('fromCurrency');
+const toCurrency = document.getElementById('toCurrency');
+const amount = document.getElementById('amount');
+
 function getExchangeRate() {
-    const fromCurrency = document.getElementById('fromCurrency').value;
-    const toCurrency = document.getElementById('toCurrency').value;
-    const amount = document.getElementById('amount').value;
+
 
     const apiKey = 'YOUR_API_KEY';
-    const apiUrl = `https://open.er-api.com/v6/latest/${fromCurrency}?apikey=${apiKey}`;
+    const apiUrl = `https://open.er-api.com/v6/latest/${fromCurrency.value}`;
 
     fetch(apiUrl)
       .then(response => response.json())
@@ -15,3 +17,7 @@ function getExchangeRate() {
       })
       .catch(error => console.error('Error fetching exchange rates:', error));
   }
+
+fromCurrency.addEventListener("input",getExchangeRate)
+amount.addEventListener("input",getExchangeRate)
+toCurrency.addEventListener("input",getExchangeRate)
