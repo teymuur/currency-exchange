b0aaedebaf7dc6365fe7cd64ce311fd41b92c747
teymuur <teymur_babayev08@yahoo.com>
2023-11-18 14:17:51 +0400

    Updated script.js

--- script.js
+++ script.js
@@ -18,6 +18,27 @@ function getExchangeRate() {
       .catch(error => console.error('Error fetching exchange rates:', error));
   }
 
+  function addCurrencyOptions(currencyCodes) {
+    const fromCurrencySelect = document.getElementById('fromCurrency');
+    const toCurrencySelect = document.getElementById('toCurrency');
+
+    // Loop through the array of currency codes
+    currencyCodes.forEach(currencyCode => {
+      // Check if the option already exists
+      if (!fromCurrencySelect.querySelector(`option[value="${currencyCode}"]`)) {
+        // Create option element
+        const option = document.createElement('option');
+        option.value = currencyCode;
+        option.text = currencyCode;
+
+        // Add option to both select elements
+        fromCurrencySelect.add(option.cloneNode(true));
+        toCurrencySelect.add(option);
+      }
+    });
+  }
+  const currencies = ['CAD', 'INR', 'CNY', 'JPY', 'GBP', 'AUD'];
+  addCurrencyOptions(currencies);
 fromCurrency.addEventListener("input",getExchangeRate)
 amount.addEventListener("input",getExchangeRate)
 toCurrency.addEventListener("input",getExchangeRate)
